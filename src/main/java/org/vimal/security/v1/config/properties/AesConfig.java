27c445e3518ad84448c4cdcc34f19ca932b9dcf2
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-22 21:01:47 +0530

    .

--- src/main/java/org/vimal/security/v1/config/properties/AesConfig.java
+++ src/main/java/org/vimal/security/v1/config/properties/AesConfig.java
@@ -8,9 +8,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @Setter
 public class AesConfig {
-    private String email;
-    private String realEmail;
-    private String username;
+//    private String email;
+//    private String realEmail;
+//    private String username;
     private String emailVerificationToken;
     private String emailVerificationToken2;
     private String jwtId;
@@ -24,7 +24,7 @@ public class AesConfig {
     private String authAppSecret;
     private String stateToken;
     private String stateToken2;
-    private String password;
+//    private String password;
     private String forEmailOtpForgotPwd;
     private String forEmailOtpForgotPwd2;
     private String forEmailOtpEmailChange;
@@ -33,6 +33,6 @@ public class AesConfig {
     private String forEmailStoreEmailChange2;
     private String forEmailOtpDeleteAccount;
     private String forEmailOtpDeleteAccount2;
-    private String createdBy;
-    private String updatedBy;
+//    private String createdBy;
+//    private String updatedBy;
 }
