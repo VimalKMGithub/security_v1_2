a27b40d8fa22d81043572eb4df92bdfa62f5c300
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-22 21:27:13 +0530

    .

--- src/main/java/org/vimal/security/v1/converter/AuthAppSecretConverter.java
+++ src/main/java/org/vimal/security/v1/converter/AuthAppSecretConverter.java
@@ -1,10 +1,10 @@
 package org.vimal.security.v1.converter;
 
-import jakarta.persistence.Converter;
+import org.springframework.stereotype.Component;
 import org.vimal.security.v1.util.aes.AESOperationType;
 import org.vimal.security.v1.util.aes.factory.AESUtilFactory;
 
-@Converter
+@Component
 public class AuthAppSecretConverter extends AbstractConverter<String> {
     public AuthAppSecretConverter(AESUtilFactory aesUtilFactory) {
         super(aesUtilFactory, AESOperationType.AUTH_APP_SECRET);
